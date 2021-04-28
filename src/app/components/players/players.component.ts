83ad77b2bf4ace792dce7ac787a8be35a30e99b8
unknown <bartlomiej.wyszecki2@gmail.com>
2021-04-28 20:39:43 +0200

    Added loader to players component

--- src/app/components/players/players.component.ts
+++ src/app/components/players/players.component.ts
@@ -12,7 +12,7 @@ import { Player } from './../../models/players';
   styleUrls: ['./players.component.scss']
 })
 export class PlayersComponent implements OnInit {
-  players!: Player<string>[];
+  loaded: boolean = false;
 
   displayedColumns: string[] = ['name', 'teamId', 'number', 'position', 'heightInMeters', 'weightInKilograms', 'country'];
   dataSource = new MatTableDataSource();
@@ -23,17 +23,20 @@ export class PlayersComponent implements OnInit {
   constructor(private http: PlayersService) { }
 
   ngOnInit(): void {
-    // this.http.getPlayers().subscribe(players => {
-    //   this.dataSource.data = players.api.players.filter((player: any) => player.teamId > 0 
-    //   && _actualTeams.includes(player.teamId)
-    //   && player.leagues.standard.active != 0
-    //   && parseInt(player.startNba) >= 1999
-    //   && player.heightInMeters > 0
-    //   && player.weightInKilograms > 0
-    //   && player.country !== ' ');
-    //   setTimeout(() => this.dataSource.paginator = this.paginator);
-    //   setTimeout(() => this.dataSource.sort = this.sort);
-    // })
+    this.http.getPlayers().subscribe(players => {
+      this.dataSource.data = players.api.players.filter((player: any) => player.teamId > 0 
+      && _actualTeams.includes(player.teamId)
+      && player.leagues.standard.active != 0
+      && parseInt(player.startNba) >= 1999
+      && player.heightInMeters > 0
+      && player.weightInKilograms > 0
+      && player.country !== ' ');
+
+      setTimeout(() => this.dataSource.paginator = this.paginator);
+      setTimeout(() => this.dataSource.sort = this.sort);
+
+      this.loaded = true;
+    })
   }
 
   searchFor(value: string) {
