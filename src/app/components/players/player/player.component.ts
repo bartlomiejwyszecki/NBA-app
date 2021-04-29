726675319da71a707824b40c2fd7ef3c8070aeef
unknown <bartlomiej.wyszecki2@gmail.com>
2021-04-29 23:41:10 +0200

    Finished player component, added routing to players component that is redirecting to specific player stats.

--- src/app/components/players/player/player.component.ts
+++ src/app/components/players/player/player.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Player } from 'src/app/models/players';
 import { PlayersService } from 'src/app/services/players/players.service';
 
 @Component({
@@ -9,6 +10,7 @@ import { PlayersService } from 'src/app/services/players/players.service';
 })
 export class PlayerComponent implements OnInit {
   stats: any;
+  player!: Player<string>;
 
   constructor(private route: ActivatedRoute, private http: PlayersService) { }
 
@@ -18,14 +20,13 @@ export class PlayerComponent implements OnInit {
     const month = new Date().getMonth()
     let season
 
-    console.log(new Date().getFullYear(), new Date().getMonth())
     this.http.getPlayer(param).subscribe(player => {
       let PLAYER = player.api.players[0]
+      this.player = player.api.players[0]
 
       this.http.getPlayersBDL(PLAYER.lastName).subscribe(players => {
           let newApiPlayerID = players.data.find((item: any) =>  item.first_name === PLAYER.firstName && 
-                                                        item.last_name === PLAYER.lastName && 
-                                                        item.position === PLAYER.leagues.standard.pos).id
+            item.last_name === PLAYER.lastName).id
 
           if (month >= 0 && month < 10) {
             season = year - 1
@@ -36,7 +37,6 @@ export class PlayerComponent implements OnInit {
           this.http.getPlayerBDLStats(season, newApiPlayerID).subscribe(res => {
             if (res.data) {
               this.stats = res.data[0]
-              console.log(this.stats)
             } else {
               console.log('There is no data for this season and this player.')
             }
