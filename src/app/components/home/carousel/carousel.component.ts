3756fcaa00f9c01f8f073fffa6a12f0b92172e77
unknown <bartlomiej.wyszecki2@gmail.com>
2021-05-10 20:32:56 +0200

    Styles ready

--- src/app/components/home/carousel/carousel.component.ts
+++ src/app/components/home/carousel/carousel.component.ts
@@ -12,4 +12,32 @@ export class CarouselComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  data = [
+    {
+      header: "Curry drains 11 3-pointers, scores 49 in Warriors' victory",
+      description: "Steph Curry lights up the Thunder for 49 points in 29 minutes and regains the lead for the scoring title while fans serenade him with chants of “MVP! MVP!”",
+      img: "https://www.gannett-cdn.com/presto/2019/08/12/USAT/ecebbf02-7aff-49df-bcc2-e4e780b88fdf-GTY_1150199611.JPG"
+    },
+    {
+      header: "Curry drains 11 3-pointers, scores 49 in Warriors' victory",
+      description: "Steph Curry lights up the Thunder for 49 points in 29 minutes and regains the lead for the scoring title while fans serenade him with chants of “MVP! MVP!”",
+      img: "https://www.gannett-cdn.com/presto/2019/08/12/USAT/ecebbf02-7aff-49df-bcc2-e4e780b88fdf-GTY_1150199611.JPG"
+    },
+    {
+      header: "Curry drains 11 3-pointers, scores 49 in Warriors' victory",
+      description: "Steph Curry lights up the Thunder for 49 points in 29 minutes and regains the lead for the scoring title while fans serenade him with chants of “MVP! MVP!”",
+      img: "https://www.gannett-cdn.com/presto/2019/08/12/USAT/ecebbf02-7aff-49df-bcc2-e4e780b88fdf-GTY_1150199611.JPG"
+    },
+    {
+      header: "Curry drains 11 3-pointers, scores 49 in Warriors' victory",
+      description: "Steph Curry lights up the Thunder for 49 points in 29 minutes and regains the lead for the scoring title while fans serenade him with chants of “MVP! MVP!”",
+      img: "https://www.gannett-cdn.com/presto/2019/08/12/USAT/ecebbf02-7aff-49df-bcc2-e4e780b88fdf-GTY_1150199611.JPG"
+    },
+    {
+      header: "Curry drains 11 3-pointers, scores 49 in Warriors' victory",
+      description: "Steph Curry lights up the Thunder for 49 points in 29 minutes and regains the lead for the scoring title while fans serenade him with chants of “MVP! MVP!”",
+      img: "https://www.gannett-cdn.com/presto/2019/08/12/USAT/ecebbf02-7aff-49df-bcc2-e4e780b88fdf-GTY_1150199611.JPG"
+    }
+  ]
+
 }
